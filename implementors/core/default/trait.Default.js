9b2ee6a94e4e0884b50834c7b3068e25ec6ac74e
Travis CI User <travis@example.org>
2018-04-09 03:52:45 +0000

    Documentation for Aatch/ramp@5367b3d

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["ramp"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"ramp/int/struct.Int.html\" title=\"struct ramp::int::Int\">Int</a>",synthetic:false,types:["ramp::int::Int"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"ramp/rational/struct.Rational.html\" title=\"struct ramp::rational::Rational\">Rational</a>",synthetic:false,types:["ramp::rational::Rational"]},];
+implementors["rand"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rand/reseeding/struct.ReseedWithDefault.html\" title=\"struct rand::reseeding::ReseedWithDefault\">ReseedWithDefault</a>",synthetic:false,types:["rand::reseeding::ReseedWithDefault"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["ramp"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"ramp/int/struct.ParseIntError.html\" title=\"struct ramp::int::ParseIntError\">ParseIntError</a>",synthetic:false,types:["ramp::int::ParseIntError"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"ramp/rational/struct.ParseRationalError.html\" title=\"struct ramp::rational::ParseRationalError\">ParseRationalError</a>",synthetic:false,types:["ramp::rational::ParseRationalError"]},];
+implementors["rand"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"rand/jitter/enum.TimerError.html\" title=\"enum rand::jitter::TimerError\">TimerError</a>",synthetic:false,types:["rand::jitter::TimerError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
